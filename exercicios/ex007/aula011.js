ef3bb54649652709dfdb706477cb86e6579ced2a
cauansaile1 <cauansaile1@gmail.com>
2025-03-11 11:08:33 -0300

    condições

---
+++ exercicios/ex007/aula011.js
@@ -0,0 +1,6 @@
+var vel = 60.01
+console.log(`Sua velocidade é ${vel}`)
+if(vel > 60){
+    console.log("Você tá pilotando rápido demais, tome uma multinha!")
+}
+console.log("Dirija com cuidado, use o cinto")
---
+++ exercicios/ex007/cond001.js
@@ -0,0 +1,7 @@
+var pais = "França"
+if(pais == "Brasil"){
+    console.log("Tu é nosso")
+}
+else{
+    console.log("Tu  é Alemão")
+}
