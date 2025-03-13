78b585a0d5b858f032de93b043c02be7ae4fc30d
cauansaile1 <cauansaile1@gmail.com>
2025-03-13 10:18:07 -0300

    mais exercícios

---
+++ exercicios/ex008/script003.js
@@ -0,0 +1,26 @@
+var dia = new Date()
+var diaSem = dia.getDay()
+
+switch (diaSem) {
+    case 0:
+        console.log("Domingo")
+        
+    case 1:
+        console.log("Segunda")
+            
+    case 2:
+        console.log("Terça")
+             
+    case 3:
+        console.log("Quarta")
+        
+    case 4:
+        console.log("Quinta")
+            
+    case 5:
+        console.log("Sexta")
+         
+    case 6:
+        console.log("Sábado")
+                     
+}
