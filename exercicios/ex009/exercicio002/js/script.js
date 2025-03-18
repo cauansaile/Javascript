f4dda52f8ef38e9e043553a3cbab587874fe7e9e
cauansaile1 <cauansaile1@gmail.com>
2025-03-18 10:25:06 -0300

    iniciei exercicio 02

---
+++ exercicios/ex009/exercicio002/js/script.js
@@ -0,0 +1,10 @@
+function verificar(){
+    var masc = document.getElementById('imasc')
+    var fem = document.getElementById('ifem')
+    var ano = document.getElementById('iano')
+    ano = Number(iano.value)
+    var idade = 2025 - ano
+    if (idade <= 15){
+        
+    }
+}
