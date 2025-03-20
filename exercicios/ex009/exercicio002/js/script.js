8bee35ed3df0e72bd8fe406bcb3bb0cdce55448c
cauansaile1 <cauansaile1@gmail.com>
2025-03-20 09:30:25 -0300

    finalizei o exercício 02

--- exercicios/ex009/exercicio002/js/script.js
+++ exercicios/ex009/exercicio002/js/script.js
@@ -4,7 +4,35 @@ function verificar(){
     var ano = document.getElementById('iano')
     ano = Number(iano.value)
     var idade = 2025 - ano
-    if (idade <= 15){
-        
+    var msg = document.getElementById('mensagem')
+    var imagem = document.getElementById('foto')
+    if (idade <= 15 && masc.checked == true) {
+        msg.innerHTML = `Você em um menino de ${idade} anos`
+        imagem.src = 'imagens/homem01.png'
     }
+    else if (idade <= 15 && fem.checked == true) {
+        msg.innerHTML = `Você é uma menina de ${idade} anos`
+        imagem.src = 'imagens/mulher01.png'
+
+    }
+    else if (idade <=64 && masc.checked == true) {
+        msg.innerHTML = `Você é um homem de ${idade} anos`
+        imagem.src = 'imagens/homem02.png'
+    }
+    else if (idade <=64 && fem.checked == true) {
+        msg.innerHTML = `Você é uma mulher de ${idade} anos`
+        imagem.src = 'imagens/mulher02.png'
+    }
+    else if (idade > 64 && masc.checked == true) {
+        msg.innerHTML = `Você é um homem de ${idade} anos`
+        imagem.src = 'imagens/homem03.png'
+    }
+    else if (idade > 64 && fem.checked == true) {
+        msg.innerHTML = `Você é uma mulher de ${idade} anos`
+        imagem.src = 'imagens/mulher03.png'
+        }
+    else{
+        msg.innerHTML = `Detectamos um erro, preencha corretamente!`
+    }    
+
 }
