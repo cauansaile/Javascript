77136cc723958822eb440f3b7a6f7e1bcddb77f4
cauansaile1 <cauansaile1@gmail.com>
2025-04-01 07:34:28 -0300

    update script.js

--- exercicios/ex014/exercicio01/modelo/js/script.js
+++ exercicios/ex014/exercicio01/modelo/js/script.js
@@ -1,16 +1,20 @@
 
+
 function add(){
 let n1 = document.getElementById('num')
 let tela = document.getElementById('lista')
+let n2 = Number(n1.value)
+let valores = [n2]
 if(n1.value.length == 0 || n1.value >100){
     window.alert('Por favor, digite um número válido!')
 }else{
-    let n2 = Number(n1.value)
-    let valores = [n2]
+    
     for(let pos in valores){
         let item = document.createElement ('option')
         item.text = `valor ${n2} adicionado`
         tela.appendChild(item)
+        
+    
         
 
 
@@ -19,8 +23,11 @@ if(n1.value.length == 0 || n1.value >100){
 }function finalizar(){
     let n1 = document.getElementById('num')
     let n2 = Number(n1.value)
-    let valores = [n2]
+    let valores = []
+    valores.push(n2)
     let res = document.getElementById('resultado')
-    res.innerHTML = `A lista contém ${valores.length} itens.`
+    let total = valores.length
+    res.innerHTML = `A lista contém ${total} itens.`
+
 }
 
