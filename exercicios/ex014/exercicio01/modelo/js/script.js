2fcb1006b9a3280ac468f89657b2f771098d2b97
cauansaile1 <cauansaile1@gmail.com>
2025-03-31 21:32:29 -0300

    inicio ex

---
+++ exercicios/ex014/exercicio01/modelo/js/script.js
@@ -0,0 +1,26 @@
+
+function add(){
+let n1 = document.getElementById('num')
+let tela = document.getElementById('lista')
+if(n1.value.length == 0 || n1.value >100){
+    window.alert('Por favor, digite um número válido!')
+}else{
+    let n2 = Number(n1.value)
+    let valores = [n2]
+    for(let pos in valores){
+        let item = document.createElement ('option')
+        item.text = `valor ${n2} adicionado`
+        tela.appendChild(item)
+        
+
+
+        }
+    }
+}function finalizar(){
+    let n1 = document.getElementById('num')
+    let n2 = Number(n1.value)
+    let valores = [n2]
+    let res = document.getElementById('resultado')
+    res.innerHTML = `A lista contém ${valores.length} itens.`
+}
+
