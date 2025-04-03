e241410d7ec165b0cf2137f09d6f08db1661cdaf
cauansaile1 <cauansaile1@gmail.com>
2025-04-03 12:04:50 -0300

    update script

--- exercicios/ex012/ambiente03.js
+++ exercicios/ex012/ambiente03.js
@@ -1,4 +1,9 @@
-let site = ["casa", "carro", "cama"]
+/*let site = ["casa", "carro", "cama"]
 let input = "casa"
 let resultado = site.indexOf(input)
-console.log(site[resultado])
+console.log(site[resultado])*/
+
+let valores = [1, 2, 20, 3 ,50 , 5, 10, 40, 25]
+console.log(valores)
+valores.sort()
+console.log(valores)
--- exercicios/ex014/exercicio01/modelo/js/script.js
+++ exercicios/ex014/exercicio01/modelo/js/script.js
@@ -1,7 +1,11 @@
 let n1 = document.getElementById('num')
 let lista = document.getElementById('lista')
 let valores = []
-let res = document.getElementById('resultado')
+let res1 = document.getElementById('resultado1')
+let res2 = document.getElementById('resultado2')
+let res3 = document.getElementById('resultado3')
+let res4 = document.getElementById('resultado4')
+let res5 = document.getElementById('resultado5')
 
 function isNumero(n){
     if(Number(n) >=1 && Number(n) <= 100){
@@ -25,15 +29,35 @@ function add(){
         let item = document.createElement ('option')
         item.text = `valor ${n1.value} adicionado`
         lista.appendChild(item)
+        c = n1.value
+        soma = c + n1.value
     
     
     }else{
     window.alert('Número inválido ou já encontrado na lista')
-}}
+
+    
+}
+    n1.value = ' '
+    n1.focus()
+}
 
 function finalizar(){
-    let total = valores.length
-    res.innerHTML = `A lista contém ${total} itens.
+    valores.sort()
+    let qtlista = valores.length
+    let maiorn = valores[valores.length - 1]
+    let menorn = valores[0]
+    if(qtlista == 1){
+        res1.innerHTML = `A lista contém 1 valor cadstrado apenas `
+    }else{
+        res1.innerHTML = `A lista contém ${qtlista} valores cadastrados `
+        res2.innerHTML = `O maior número cadastrado foi ${maiorn}`
+        res3.innerHTML = `O menor número cadastrado foi ${menorn}`
+        res4.innerHTML = `A soma dos valores cadastrados foi de ${soma}`
+    }
+
+    
+    
 }
     
 
